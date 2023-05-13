dcea6c87990b5557952dacf1a738c30c234760dd
Gabriel da Cruz Bogo <101855454+GabrielBogo1@users.noreply.github.com>
2023-05-13 14:07:51 -0300

    Delete AdministradorRepository.java

--- AdministradorRepository.java
+++
@@ -1,8 +0,0 @@
-package cci.projetointegrador.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface Administrador extends JpaRepository <cci.projetointegrador.entity.Administrador,Long> {
-}
