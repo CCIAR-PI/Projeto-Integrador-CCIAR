736c00b8c6ba624b6630fe28901af470bb63a8b7
Samuel Heidner Gomes Medeiros Leal <116127411+SamHeidner@users.noreply.github.com>
2023-05-15 19:28:42 -0300

    Validação de Rg(atualização)
    adicionando comentarios para entendimento

--- projetointegrador/src/main/java/cci/projetointegrador/configs/ValidaRg.java
+++ projetointegrador/src/main/java/cci/projetointegrador/configs/ValidaRg.java
@@ -7,20 +7,20 @@ import org.springframework.stereotype.Component;
 
 public class ValidaRg {
     public static boolean validarRG(String rg) {
-
+        // Remove possíveis caracteres especiais do RG (pontuação, espaços, etc.)
         rg = rg.replaceAll("[^0-9]", "");
 
-
+        // Verifica se o RG tem 9 dígitos
         if (rg.length() != 9) {
             return false;
         }
 
-
+        // Verifica se todos os dígitos do RG são iguais (ex: 111111111)
         if (rg.matches("(\\d)\\1+")) {
             return false;
         }
 
-
+        // Calcula o dígito verificador do RG
         int soma = 0;
         int peso = 2;
         for (int i = rg.length() - 2; i >= 0; i--) {
@@ -30,7 +30,7 @@ public class ValidaRg {
 
         int digitoVerificador = 11 - (soma % 11);
 
-
+        // Verifica se o dígito verificador é válido
         if (digitoVerificador >= 10) {
             digitoVerificador = 0;
         }
@@ -40,7 +40,7 @@ public class ValidaRg {
     }
 
     public static void main(String[] args) {
-        String rg = "123456789";
+        String rg = "123456789"; // Substitua pelo RG a ser validado
 
         if (validarRG(rg)) {
             System.out.println("RG válido!");
