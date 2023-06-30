8bfaf6c34c3b5247480cc57581c5aae06b92311a
Gabriel Bogo <gabrieldacruzbogo01@gmail.com>
2023-06-30 20:20:12 -0300

    alterando a editaPessoa e corrigindo o deletePessoa

--- projetointegrador/src/main/java/cci/projetointegrador/service/PessoaService.java
+++ projetointegrador/src/main/java/cci/projetointegrador/service/PessoaService.java
@@ -79,18 +79,64 @@ public class PessoaService {
     }
 
     @Transactional (rollbackFor = Exception.class)
-    public void editarPessoa (final Long id){
+    public void editarPessoa (final Long id, final Pessoa pessoa){
+
         final Pessoa pessoaBanco = this.pessoaRepository.findById(id).orElse(null);
 
         if (pessoaBanco == null || !pessoaBanco.getId().equals(id)){
             throw new RuntimeException("Não foi possível identificar o registro informado");
         }
 
+        Assert.isTrue(!pessoa.getNome().equals(""), "Nome não pode ser nulo");
+        Assert.isTrue(pessoa.getNome().length() <= 50, "Nome acima do limite de caracteres");
+
+        Assert.isTrue(!pessoa.getCpf().equals(""),"CPF não pode ser nulo");
+
+
+        Assert.isTrue(pessoa.getDataNascimento() >= 0, "Data de nascimento não pode ser nula");
+
+        Assert.isTrue(!pessoa.getRg().equals("") , "Rg não pode ser nulo");
+        Assert.isTrue(pessoa.getRg().length() <= 9, "Rg inválido");
+
+        Assert.isTrue(!pessoa.getTelefoneEmergencia().equals(""), "Telefone não pode ser nulo");
+
+
+
+        Assert.isTrue(!pessoa.getNaturalidade().equals(""), "Naturalidade não pode ser nula");
+        Assert.isTrue(pessoa.getNaturalidade().length() <= 50, "Naturalidade acima do limite de caracteres");
+
+        Assert.isTrue(!pessoa.getNacionalidade().equals(""), "Nacionalidade não pode ser nula");
+        Assert.isTrue(pessoa.getNacionalidade().length() <= 50, "Nacionalidade acima do limite de caracteres");
+
+        Assert.isTrue(!pessoa.getRua().equals(""), "RUA não pode ser nulo");
+        Assert.isTrue(pessoa.getRua().length() <= 50, "Rua acima do limite de caracteres");
+
+        Assert.isTrue(!pessoa.getBairro().equals(""), "Bairro não pode ser nulo");
+        Assert.isTrue(pessoa.getBairro().length() <= 50, "Bairro acima do limite de caracteres");
+
+        Assert.isTrue(!pessoa.getNaturalidade().equals(""), "Campo Naturalidade não pode ser nula");
+        Assert.isTrue(pessoa.getNaturalidade().length() <= 50, "Campo Naturalidade acima do limite de caracteres");
+
+
+        Assert.isTrue(!pessoa.getRua().equals(""), "Campo RUA não pode ser nulo");
+        Assert.isTrue(pessoa.getRua().length() <= 50, "Campo RUA acima do limite de caracteres");
+
+        Assert.isTrue(!pessoa.getBairro().equals(""), "Campo Bairro não pode ser nulo");
+        Assert.isTrue(pessoa.getBairro().length() <= 50, "Campo Bairro acima do limite de caracteres");
+
+
+
+        Assert.isTrue(pessoa.getNumeroCasa() >= 0, "Número da casa não pode ser nulo");
+
+        Assert.isTrue(!pessoa.getCep().equals(""), "CEP não pode ser nulo");
+        Assert.isTrue(pessoa.getRua().length() <= 9 , "CEP inválido");
+
         this.pessoaRepository.save(pessoaBanco);
     }
 
     @Transactional (rollbackFor = Exception.class)
-    public void deletarPessoa (final Long id){
+    public void deletarPessoa (final Long id, final Pessoa pessoa){
+        pessoa.setAtivo(false);
         final Pessoa pessoaBanco = this.pessoaRepository.findById(id).orElse(null);
 
         if (pessoaBanco == null || !pessoaBanco.getId().equals(id)){
