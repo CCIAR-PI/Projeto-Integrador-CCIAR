0b1a4f5dbec9a2143e20eb6ab26d610e18e81a88
Gabriel Bogo <gabrieldacruzbogo01@gmail.com>
2023-06-30 21:07:18 -0300

    modificando escolaridade Enum

---
+++ src/model/EscolaridadeModel.ts
@@ -0,0 +1,10 @@
+export enum Escolaridade {
+    // EnFundamentalIncompleto, EnFundamentalCompleto, EnMedioIncompleto, EnMedioCompleto,Cursando, EnSuperiorComp
+    EnFundamentalIncompleto = 'EnFundamentalIncompleto',
+    EnFundamentalCompleto = 'EnFundamentalCompleto',
+    EnMedioIncompleto = 'EnMedioIncompleto',
+    EnMedioCompleto = 'EnMedioCompleto',
+    Cursando = 'Cursando',
+    EnSuperiorComp = 'EnSuperiorComp'
+
+}
