ecf379c8c2c4230adbdd41c13cd4169448477b99
Gabriel Bogo <gabrieldacruzbogo01@gmail.com>
2023-06-30 22:14:05 -0300

    configurando o router

--- src/router/index.ts
+++ src/router/index.ts
@@ -14,6 +14,75 @@ const routes: Array<RouteRecordRaw> = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/pessoa/lista',
+    name: 'pessoa-lista-view',
+    component: () => import(/* webpackChunkName: "about" */ '@/views/Pessoa/PessoaList.vue')
+  },
+
+  {
+    path: '/pessoa/formulario',
+    name: 'pessoa-formulario-view',
+    component: () => import(/* webpackChunkName: "about" */ '@/views/Pessoa/PessoaForm.vue'),
+    children: [
+      {
+        path: '/pessoa/formulario',
+        name: 'pessoa-formulario-editar-view',
+        component: () => import(/* webpackChunkName: "about" */ '@/views/Pessoa/PessoaForm.vue'),
+      },
+      {
+        path: '/pessoa/formulario',
+        name: 'pessoa-formulario-excluir-view',
+        component: () => import(/* webpackChunkName: "about" */ '@/views/Pessoa/PessoaForm.vue')
+      }
+    ]
+  },
+  {
+    path: '/admimistrador/lista',
+    name: 'administrador-lista-view',
+    component: () => import(/* webpackChunkName: "about" */ '@/views/Administrador/AdministradorList.vue')
+  },
+
+  {
+    path: '/administrador/formulario',
+    name: 'administrador-formulario-view',
+    component: () => import(/* webpackChunkName: "about" */ '@/views/Administrador/AdministradorForm.vue'),
+    children: [
+      {
+        path: '/administrador/formulario',
+        name: 'administrador-formulario-editar-view',
+        component: () => import(/* webpackChunkName: "about" */ '@/views//Administrador/AdministradorForm.vue'),
+      },
+      {
+        path: '/administrador/formulario',
+        name: 'administrador-formulario-excluir-view',
+        component: () => import(/* webpackChunkName: "about" */ '@/views/Administrador/AdministradorForm.vue')
+      }
+    ]
+  },
+  {
+    path: '/atividade/lista',
+    name: 'atividade-lista-view',
+    component: () => import(/* webpackChunkName: "about" */ '@/views/Atividades/AtividadesList.vue')
+  },
+
+  {
+    path: '/atividade/formulario',
+    name: 'atividade-formulario-view',
+    component: () => import(/* webpackChunkName: "about" */ '@/views/Atividades/AtividadesForm.vue'),
+    children: [
+      {
+        path: '/atividade/formulario',
+        name: 'atividade-formulario-editar-view',
+        component: () => import(/* webpackChunkName: "about" */ '@/views/Atividades/AtividadesForm.vue'),
+      },
+      {
+        path: '/atividade/formulario',
+        name: 'atividade-formulario-excluir-view',
+        component: () => import(/* webpackChunkName: "about" */ '@/views/Atividades/AtividadesForm.vue')
+      }
+    ]
   }
 ]
 
