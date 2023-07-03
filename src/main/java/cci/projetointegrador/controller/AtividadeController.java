c4c8f77b369040451fce741461a717806fa32611
PedroKklaus <pedrocaklaus14@gmail.com>
2023-07-03 19:01:04 -0300

    pedro-klaus

--- src/main/java/cci/projetointegrador/controller/AtividadeController.java
+++ src/main/java/cci/projetointegrador/controller/AtividadeController.java
@@ -10,7 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+@Controller //agenda controller
 @RequestMapping(value = "/api/atividade")
 
 public class AtividadeController {
