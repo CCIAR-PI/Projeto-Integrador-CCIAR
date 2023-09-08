900fc8e70b6eefe436b485ffb84d676536fd58c7
Gabriel Bogo <gabrieldacruzbogo01@gmail.com>
2023-09-07 23:12:31 -0300

    adicionando o teste de pessoas

--- src/test/java/projetointegrador/cciar/projetointegradorcciar/ProjetointegradorCciarApplicationTests.java
+++ src/test/java/projetointegrador/cciar/projetointegradorcciar/ProjetointegradorCciarApplicationTests.java
@@ -4,7 +4,10 @@ import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import projetointegrador.cciar.projetointegradorcciar.entity.Endereco;
+import projetointegrador.cciar.projetointegradorcciar.entity.Escolaridade;
 import projetointegrador.cciar.projetointegradorcciar.entity.Pessoa;
+import projetointegrador.cciar.projetointegradorcciar.entity.Sexo;
 import projetointegrador.cciar.projetointegradorcciar.service.PessoaService;
 
 @SpringBootTest
@@ -20,10 +23,38 @@ class ProjetointegradorCciarApplicationTests {
 	@Test
 	public void testPessoa (){
 		Pessoa pessoa = new Pessoa();
+		Endereco endereco = new Endereco();
+
+		endereco.setUf("PR");
+		endereco.setCep("44095-160");
+		endereco.setBairro("testeBairro");
+		endereco.setLogradouro("testeLogradouro");
+		endereco.setNumCasa(123);
+		endereco.setLocalidade("testeLocalidade");
+
 		pessoa.setNome("Teste");
+		pessoa.setRg("14.215.823-9");
+		pessoa.setCpf("202.652.050-00");
+		pessoa.setEndereco(endereco);
+		pessoa.setSexo(Sexo.MASCULINO);
+		pessoa.setCadastroPor("TesteCadastro");
+		pessoa.setEscolaridade(Escolaridade.Cursando);
+		pessoa.setTelefone("(45)89989-8900");
+		pessoa.setNacionalidade("Brasileiro");
+		pessoa.setNaturalidade("Paulista");
+		pessoa.setDataNascimento(2004);
 
-		Assert.assertEquals("pao", pessoa.getNome());
-
+		Assert.assertEquals("Teste", pessoa.getNome());
+		Assert.assertEquals("14.215.823-9", pessoa.getRg());
+		Assert.assertEquals("202.652.050-00", pessoa.getCpf());
+		Assert.assertEquals(endereco, pessoa.getEndereco());
+		Assert.assertEquals(Sexo.MASCULINO, pessoa.getSexo());
+		Assert.assertEquals("TesteCadastro", pessoa.getCadastroPor());
+		Assert.assertEquals(Escolaridade.Cursando, pessoa.getEscolaridade());
+		Assert.assertEquals("(45)89989-8900", pessoa.getTelefone());
+		Assert.assertEquals("Brasileiro", pessoa.getNacionalidade());
+		Assert.assertEquals("Paulista", pessoa.getNaturalidade());
+		Assert.assertEquals(2004, pessoa.getDataNascimento());
 	}
 
 }
