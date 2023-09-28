dcbf411ca4732d5e68e16a25affda8ccefd29deb
Gabriel Bogo <gabrieldacruzbogo01@gmail.com>
2023-09-28 19:50:42 -0300

    adicionando alguns testes unitarios em pessoa

--- src/test/java/projetointegrador/cciar/projetointegradorcciar/unitTests/PessoaTests.java
+++ src/test/java/projetointegrador/cciar/projetointegradorcciar/unitTests/PessoaTests.java
@@ -10,6 +10,8 @@ import projetointegrador.cciar.projetointegradorcciar.entity.Escolaridade;
 import projetointegrador.cciar.projetointegradorcciar.entity.Pessoa;
 import projetointegrador.cciar.projetointegradorcciar.entity.Sexo;
 
+import java.time.LocalDateTime;
+
 @SpringBootTest
 class PessoaTests {
 
@@ -18,26 +20,118 @@ class PessoaTests {
 
 
     @Test
-    @DisplayName("Testa se o atributo nome está recebendo o valor esperado e se não está retornando nulo")
-    void validaCadastroNome (){
+    @DisplayName("Testa se o atributo nome está recebendo o valor esperado")
+    void validaNome (){
         Pessoa pessoa = new Pessoa ();
         pessoa.setNome("nomeTeste");
         Assertions.assertEquals("nomeTeste", pessoa.getNome());
     }
 
     @Test
-    @DisplayName("Testa se o atributo nome está recebendo o valor esperado e se não está retornando nulo")
-    void validaCadastroNomeNulo (){
+    @DisplayName("Testa se o atributo nome não está retornando nulo")
+    void validaoNomeNulo (){
         pessoaDTO.setNome("nomeTeste");
         Assertions.assertNotNull(pessoaDTO.getNome());
     }
 
     @Test
-    void validaCadastroCpf (){
+    void validaCpf (){
         pessoaDTO.setCpf("117.283.639-69");
         Assertions.assertEquals("117.283.639-69", pessoaDTO.getCpf());
-
     }
+
+    @Test
+    void validaCpfNulo (){
+        pessoaDTO.setCpf("117.283.639-69");
+        Assertions.assertNotNull(pessoaDTO.getCpf());
+    }
+
+    @Test
+    void validaRg (){
+        pessoaDTO.setRg("49.630.826-9");
+        Assertions.assertEquals("49.630.826-9", pessoaDTO.getRg());
+    }
+
+    @Test
+    void validaRgNulo (){
+        pessoaDTO.setRg("49.630.826-9");
+        Assertions.assertNotNull(pessoaDTO.getRg());
+    }
+
+    @Test
+    void validaTelefone (){
+        pessoaDTO.setTelefone("(45)99965-6820");
+        Assertions.assertEquals("(45)99965-6820", pessoaDTO.getTelefone());
+    }
+
+    @Test
+    void validaTelefoneNulo (){
+        pessoaDTO.setTelefone("(45)99965-6820");
+        Assertions.assertNotNull(pessoaDTO.getTelefone());
+    }
+
+    @Test
+    void validaNacionalidade (){
+        pessoaDTO.setNacionalidade("Brasileiro");
+        Assertions.assertEquals("Brasileiro", pessoaDTO.getNacionalidade());
+    }
+
+    @Test
+    void validaNacionalidadeNula (){
+        pessoaDTO.setNacionalidade("Brasileiro");
+        Assertions.assertNotNull(pessoaDTO.getNacionalidade());
+    }
+
+    @Test
+    void validaNaturalidade (){
+        pessoaDTO.setNaturalidade("Paranaense");
+        Assertions.assertEquals("Paranaense", pessoaDTO.getNaturalidade());
+    }
+
+    @Test
+    void validaNaturalidadeNula (){
+        pessoaDTO.setNaturalidade("Paranaense");
+        Assertions.assertNotNull(pessoaDTO.getNaturalidade());
+    }
+
+    @Test
+    void validaSexo(){
+        pessoaDTO.setSexo(Sexo.MASCULINO);
+        Assertions.assertEquals(Sexo.MASCULINO, pessoaDTO.getSexo());
+    }
+
+    @Test
+    void validaSexoNulo(){
+        pessoaDTO.setSexo(Sexo.MASCULINO);
+        Assertions.assertNotNull(pessoaDTO.getSexo());
+    }
+
+    @Test
+    void validaEscolaridade(){
+        pessoaDTO.setEscolaridade(Escolaridade.CURSANDO);
+        Assertions.assertEquals(Escolaridade.CURSANDO, pessoaDTO.getEscolaridade());
+    }
+
+    @Test
+    void validaEscolaridadeNula(){
+        pessoaDTO.setEscolaridade(Escolaridade.CURSANDO);
+        Assertions.assertNotNull(pessoaDTO.getEscolaridade());
+    }
+
+    @Test
+    void validaDataCadastro(){
+        pessoaDTO.setDataCadastro(LocalDateTime.now());
+        Assertions.assertEquals(LocalDateTime.now(), pessoaDTO.getDataCadastro());
+    }
+
+    @Test
+    void validaDataCadastroNula(){
+        pessoaDTO.setDataCadastro(LocalDateTime.now());
+        Assertions.assertNotNull(pessoaDTO.getDataCadastro());
+    }
+
+
+
 }
 
 
